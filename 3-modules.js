1fdde83531ef9f43683a2061700384a7726e617e
daniel <daniel@vbox.home.com>
2022-09-24 21:25:06 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+sayHi('Lisa')
+sayHi(names.john)
+sayHi(names.peter)
